ff7d2c8d7fb520a80a0eaa1ba41e51970502e97f
vc21033 <vc21033@ues.edu.sv>
2025-11-01 21:31:56 +0000

    ui: Vistas con los formularios para crear registros

--- lib/presentation/screens/authors/authors_form.dart
+++ lib/presentation/screens/authors/authors_form.dart
@@ -1,20 +1,108 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/models/author.dart';
 
 class AuthorsForm extends StatefulWidget {
-  const AuthorsForm({super.key});
+  final Author? author;
+
+  const AuthorsForm({super.key, this.author});
 
   @override
-  State<AuthorsForm> createState() => _AuthorsFormState();
+  // ignore: no_logic_in_create_state
+  State<AuthorsForm> createState() => _AuthorsFormState(author);
 }
 
 class _AuthorsFormState extends State<AuthorsForm> {
+  final TextEditingController txtName = TextEditingController();
+  final TextEditingController txtNationality = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  String message = 'Nuevo Autor';
+  final Author? authorToEdit;
+
+  _AuthorsFormState(this.authorToEdit);
+
+  @override
+  void initState() {
+    super.initState();
+    if (authorToEdit != null) {
+      txtName.text = authorToEdit!.name;
+      txtNationality.text = authorToEdit!.nationality;
+      message = 'Editar Autor';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Nuevo Autor"),
+        title: Text(message),
       ),
-      body: Text("Ingresando nuevo autor"),
+      body: Column(
+        children: [
+          SizedBox(height: 40,),
+          Text(
+            "Por favor rellene el siguiente formulario:", 
+            style: TextStyle(
+              fontWeight: FontWeight.bold, 
+              color: Colors.indigo,
+              fontSize: 18,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(50), 
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: txtName,
+                    decoration: InputDecoration(
+                      labelText: 'Nombre del autor',
+                      icon: Icon(Icons.account_box, color: Colors.blue,),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'El campo no puede ser vacío.';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 20,),
+                  TextFormField(
+                    controller: txtNationality,
+                    decoration: InputDecoration(
+                      labelText: 'Nacionalidad del autor',
+                      icon: Icon(Icons.map, color: Colors.green,),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'El campo no puede ser vacío.';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 100,),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        // Guardar
+                      }
+                    }, 
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(Icons.save),
+                        SizedBox(width: 10,),
+                        Text("Guardar")
+                      ],
+                    )
+                  )
+                ],
+              )
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/presentation/screens/books/books_form.dart
+++ lib/presentation/screens/books/books_form.dart
@@ -1,20 +1,105 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/models/book.dart';
 
 class BooksForm extends StatefulWidget {
-  const BooksForm({super.key});
+  final Book? book;
+  const BooksForm({super.key, this.book});
 
   @override
-  State<BooksForm> createState() => _BooksFormState();
+  // ignore: no_logic_in_create_state
+  State<BooksForm> createState() => _BooksFormState(book);
 }
 
 class _BooksFormState extends State<BooksForm> {
+  final TextEditingController txtTitle = TextEditingController();
+  final TextEditingController txtPublicationYear = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  String message = 'Nuevo Libro';
+  final Book? bookToEdit;
+
+  _BooksFormState(this.bookToEdit);
+
+  @override
+  void initState() {
+    super.initState();
+    if (bookToEdit != null) {
+      txtTitle.text = bookToEdit!.title;
+      txtPublicationYear.text = bookToEdit!.publicationYear;
+      message = 'Editar Libro';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Nuevo Libro"),
       ),
-      body: Text("Ingresando nuevo libro"),
+      body: Column(
+        children: [
+          SizedBox(height: 40,),
+          Text(
+            "Por favor rellene el siguiente formulario:", 
+            style: TextStyle(
+              fontWeight: FontWeight.bold, 
+              color: Colors.indigo,
+              fontSize: 18,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(50),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: txtTitle,
+                    decoration: InputDecoration(
+                      labelText: 'Titulo del libro',
+                      icon: Icon(Icons.book, color: Colors.green,)
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'El campo no puede ser vacío';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 20,),
+                  TextFormField(
+                    controller: txtPublicationYear,
+                    decoration: InputDecoration(
+                      labelText: 'Año de publicación',
+                      icon: Icon(Icons.date_range, color: Colors.pink,)
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'El campo no puede ser vacío';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 100,),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Guardar el libro xd
+                    }, 
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(Icons.save),
+                        SizedBox(width: 10,),
+                        Text("Guardar")
+                      ],
+                    )
+                  )
+                ],
+              )
+            ),
+          )
+        ],
+      ),
     );
   }
 }
