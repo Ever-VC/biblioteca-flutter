72e3db5d0242e8b45c8243eb7228839fcf482ee9
vc21033 <vc21033@ues.edu.sv>
2025-11-01 21:32:19 +0000

    iu: Vista del listado de autores

--- lib/presentation/screens/authors/authors.dart
+++ lib/presentation/screens/authors/authors.dart
@@ -1,5 +1,9 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:myapp/models/author.dart';
 import 'package:myapp/presentation/screens/authors/authors_form.dart';
+import 'package:myapp/providers/author_provider.dart';
 
 class AuthorsScreen extends StatefulWidget {
   const AuthorsScreen({super.key});
@@ -9,17 +13,60 @@ class AuthorsScreen extends StatefulWidget {
 }
 
 class _AuthorsScreenState extends State<AuthorsScreen> {
+  List<Author> lstAuthors = [];
+  bool isTimeOut = true;
+
+  Future<void> loadAuthors() async {
+    AuthorProvider authorProvider = AuthorProvider();
+    var data = await  authorProvider.getAllAuthors();
+    if (data.isNotEmpty) {
+      setState(() {
+        lstAuthors = data;
+      });
+    } else {
+      await Future.delayed(Duration(seconds: 3));
+      isTimeOut = false;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadAuthors();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Autores"),
       ),
-      body: Center(child: Text("Hola mundo desde autores"),),
+      body: Column(
+        children: [
+          Text("Listado de Autores"),
+          SizedBox(height: 30,),
+          lstAuthors.isEmpty 
+          ? (isTimeOut 
+              ? Center(child: Text("No hay autores en la base de datos"),) 
+              : Center(child: CircularProgressIndicator(),))
+          : ListView.builder(
+            itemCount: lstAuthors.length,
+            itemBuilder: (context, index) {
+              final authorTemp = lstAuthors[index];
+              return ListTile(
+                title: Text(authorTemp.name),
+                subtitle: Text(authorTemp.nationality),
+                leading: Icon(Icons.account_circle),
+              );
+            }
+          )
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           var response = await Navigator.push(context, MaterialPageRoute(builder: (context) => AuthorsForm()));
           if (response == null) {
+            loadAuthors();
             print("He regresado de autores!");
           }
         },
