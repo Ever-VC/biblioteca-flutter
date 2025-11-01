b01c7a7d10468f98f0bcea6c2f85311c57170894
Ever-VC <ever.vasquezc27@gmail.com>
2025-11-01 15:59:21 -0600

    🔨 Refactor Code:  Quitando espacios innecesarios, modificando lógica de el spinner de carga y eliminando import innecesario

--- lib/presentation/screens/authors/authors.dart
+++ lib/presentation/screens/authors/authors.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:myapp/models/author.dart';
 import 'package:myapp/presentation/screens/authors/authors_form.dart';
@@ -14,18 +12,18 @@ class AuthorsScreen extends StatefulWidget {
 
 class _AuthorsScreenState extends State<AuthorsScreen> {
   List<Author> lstAuthors = [];
-  bool isTimeOut = true;
+  bool isTimeOut = false;
 
   Future<void> loadAuthors() async {
     AuthorProvider authorProvider = AuthorProvider();
-    var data = await  authorProvider.getAllAuthors();
+    var data = await authorProvider.getAllAuthors();
     if (data.isNotEmpty) {
       setState(() {
         lstAuthors = data;
       });
     } else {
       await Future.delayed(Duration(seconds: 3));
-      isTimeOut = false;
+      isTimeOut = true;
     }
   }
 
@@ -38,33 +36,34 @@ class _AuthorsScreenState extends State<AuthorsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Autores"),
-      ),
+      appBar: AppBar(title: Text("Autores")),
       body: Column(
         children: [
           Text("Listado de Autores"),
-          SizedBox(height: 30,),
-          lstAuthors.isEmpty 
-          ? (isTimeOut 
-              ? Center(child: Text("No hay autores en la base de datos"),) 
-              : Center(child: CircularProgressIndicator(),))
-          : ListView.builder(
-            itemCount: lstAuthors.length,
-            itemBuilder: (context, index) {
-              final authorTemp = lstAuthors[index];
-              return ListTile(
-                title: Text(authorTemp.name),
-                subtitle: Text(authorTemp.nationality),
-                leading: Icon(Icons.account_circle),
-              );
-            }
-          )
+          SizedBox(height: 30),
+          lstAuthors.isEmpty
+              ? (isTimeOut
+                    ? Center(child: Text("No hay autores en la base de datos"))
+                    : Center(child: CircularProgressIndicator()))
+              : ListView.builder(
+                  itemCount: lstAuthors.length,
+                  itemBuilder: (context, index) {
+                    final authorTemp = lstAuthors[index];
+                    return ListTile(
+                      title: Text(authorTemp.name),
+                      subtitle: Text(authorTemp.nationality),
+                      leading: Icon(Icons.account_circle),
+                    );
+                  },
+                ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-          var response = await Navigator.push(context, MaterialPageRoute(builder: (context) => AuthorsForm()));
+          var response = await Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AuthorsForm()),
+          );
           if (response == null) {
             loadAuthors();
             print("He regresado de autores!");
